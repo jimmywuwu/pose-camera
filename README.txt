0eddaf508d715c184cc402feb6b1d6f129991869
Jimmy <jimmywu06211@gmail.com>
2025-05-31 17:24:13 +0800

    Add dir read

---
+++ README.txt
@@ -0,0 +1,8 @@
+--
+python version  3.12.3
+
+-- run code below
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3 experiements/photo_detect.py 
--- experiements/photo_detect.py
+++ experiements/photo_detect.py
@@ -3,7 +3,7 @@ import tensorflow_hub as hub
 from tensorflow_docs.vis import embed
 import numpy as np
 import cv2
-import pdb
+import os
 
 # Import matplotlib libraries
 from matplotlib import pyplot as plt
@@ -181,7 +181,7 @@ def draw_prediction_on_image(
 
   (keypoint_locs, keypoint_edges,
    edge_colors) = _keypoints_and_edges_for_display(
-       keypoints_with_scores, height, width, keypoint_threshold=0.5)
+       keypoints_with_scores, height, width, keypoint_threshold=0.3)
 
   line_segments.set_segments(keypoint_edges)
   line_segments.set_color(edge_colors)
@@ -226,27 +226,38 @@ def draw_prediction_on_image(
   return image_from_plot
 
 # image_path = 'input_image.jpeg'
-image_path = 'half_body.jpeg'
-image = tf.io.read_file(image_path)
-image = tf.image.decode_jpeg(image)
+
+try:
+      os.mkdir("output")
+except OSError as error:
+    print(error)
 
 
-# Resize and pad the image to keep the aspect ratio and fit the expected size.
-input_image = tf.expand_dims(image, axis=0)
-input_image = tf.image.resize_with_pad(input_image, input_size, input_size)
+for file_name in os.listdir("asset"):
+  image_path = "asset/"+file_name
+  image = tf.io.read_file(image_path)
+  image = tf.image.decode_jpeg(image)
 
-# Run model inference.
-keypoints_with_scores = movenet(input_image)
 
-# Visualize the predictions with image.
-display_image = tf.expand_dims(image, axis=0)
-display_image = tf.cast(tf.image.resize_with_pad(
-    display_image, 1280, 1280), dtype=tf.int32)
-output_overlay = draw_prediction_on_image(
-    np.squeeze(display_image.numpy(), axis=0), keypoints_with_scores)
+  # Resize and pad the image to keep the aspect ratio and fit the expected size.
+  input_image = tf.expand_dims(image, axis=0)
+  input_image = tf.image.resize_with_pad(input_image, input_size, input_size)
 
-plt.figure(figsize=(5, 5))
-plt.imshow(output_overlay)
-_ = plt.axis('off')
+  # Run model inference.
+  keypoints_with_scores = movenet(input_image)
 
-plt.show()
+  # Visualize the predictions with image.
+  display_image = tf.expand_dims(image, axis=0)
+  display_image = tf.cast(tf.image.resize_with_pad(
+      display_image, 1280, 1280), dtype=tf.int32)
+  output_overlay = draw_prediction_on_image(
+      np.squeeze(display_image.numpy(), axis=0), keypoints_with_scores)
+
+  plt.figure(figsize=(5, 5))
+  plt.imshow(output_overlay)
+  _ = plt.axis('off')
+
+  
+
+  
+  plt.savefig("output/"+file_name)
